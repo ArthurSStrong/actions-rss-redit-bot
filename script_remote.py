d3bb910444215b2ff993ced53368d64e4397bed6
ArthurShelby <seraphin.dream@gmail.com>
2020-08-25 09:55:40 -0500

    Update script_remote.py

--- script_remote.py
+++ script_remote.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 Takes the top 2 Google News and posts them to Reddit.
 """
@@ -8,14 +11,15 @@ import os
 import requests
 import praw
 
+CLIENT_ID = (os.environ['CLIENT_ID'] if 'CLIENT_ID'
+             in os.environ else '')
+CLIENT_SECRET = (os.environ['CLIENT_SECRET'] if 'CLIENT_SECRET'
+                 in os.environ else '')
+USERNAME = (os.environ['USERNAME'] if 'USERNAME' in os.environ else '')
+PASSWORD = (os.environ['PASSWORD'] if 'PASSWORD' in os.environ else '')
 
-CLIENT_ID = os.environ["CLIENT_ID"] if "CLIENT_ID" in os.environ else ''
-CLIENT_SECRET = os.environ["CLIENT_SECRET"] if "CLIENT_SECRET" in os.environ else ''
-USERNAME = os.environ["USERNAME"] if "USERNAME" in os.environ else ''
-PASSWORD = os.environ["PASSWORD"] if "PASSWORD" in os.environ else ''
-
-LOG_FILE = "./processed_urls.txt"
-NEWS_URL_FILE = "./source_urls.txt"
+LOG_FILE = './processed_urls.txt'
+NEWS_URL_FILE = './source_urls.txt'
 
 
 def load_file(file):
@@ -32,11 +36,11 @@ def load_file(file):
     """
 
     try:
-        with open(file, "r", encoding="utf-8") as temp_file:
+        with open(file, 'r', encoding='utf-8') as temp_file:
             return temp_file.read().splitlines()
-
     except Exception:
-        with open(LOG_FILE, "w", encoding="utf-8") as temp_file:
+
+        with open(LOG_FILE, 'w', encoding='utf-8') as temp_file:
             return []
 
 
@@ -52,41 +56,47 @@ def update_file(file, data):
 
     """
 
-    with open(file, "a", encoding="utf-8") as temp_file:
-        temp_file.write(data + "\n")
+    with open(file, 'a', encoding='utf-8') as temp_file:
+        temp_file.write(data + '\n')
 
 
 def init_bot():
     """Reads the RSS feed."""
 
     # We create the Reddit instance.
-    reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, username=USERNAME, password=PASSWORD, user_agent="testscript by /u/larry3000bot")
+
+    reddit = praw.Reddit(client_id=CLIENT_ID,
+                         client_secret=CLIENT_SECRET,
+                         username=USERNAME, password=PASSWORD,
+                         user_agent='testscript by /u/larry3000bot')
 
     rss_urls = load_file(NEWS_URL_FILE)
 
     for rss_url in rss_urls:
+        try:
+            with requests.get(rss_url) as response:
+                root = ET.fromstring(response.text)
 
-        with requests.get(rss_url) as response:
+                # Only read the first 3 links.
 
-            root = ET.fromstring(response.text)
+                for item in root.findall('.//item')[:3]:
+                    log = load_file(LOG_FILE)
 
-            # Only read the first 3 links.
-            for item in root.findall(".//item")[:3]:          
-                log = load_file(LOG_FILE)
+                    title = item.find('title').text.split(' - '
+                            )[0].split(' | ')[0].strip()
+                    url = item.find('link').text
 
-                title = item.find("title").text.split(" - ")[0].split(" | ")[0].strip()
-                url = item.find("link").text
-
-                if url not in log and title not in log:
-
-                    reddit.subreddit('lazonacero').submit(
-                        title=title, url=url)
-                    update_file(LOG_FILE, url)
-                    update_file(LOG_FILE, title)
-                    print("Posted:", url)
-    print("end of script")
+                    if url not in log and title not in log:
+                        reddit.subreddit('lazonacero'
+                                ).submit(title=title, url=url)
+                        update_file(LOG_FILE, url)
+                        update_file(LOG_FILE, title)
+                        print ('Posted:', url)
+        except requests.ConnectionError:
+            continue
+    print 'end of script'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     init_bot()
